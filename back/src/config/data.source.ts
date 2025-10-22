9d12956fd9529780bd59b6c432d7b354702e55d2
hectorcoro13 <hector32219@gmail.com>
2025-10-22 04:56:47 -0500

    base de datos

--- back/src/config/data.source.ts
+++ back/src/config/data.source.ts
@@ -1,21 +1,25 @@
 import {DataSource, Repository} from "typeorm"
-import { DB_DATABASE, DB_DROP, DB_ENTITES, DB_HOST, DB_LOGGING, DB_PASSWORD, DB_PORT, DB_SYNC, DB_TYPE, DB_USERNAME } from "./env"
+import { DB_DROP, DB_ENTITES, DB_LOGGING, DB_SYNC  } from "./env";
 import { User } from "../entities/User.entity"
 import { Credential } from "../entities/Credentials.entity"
 
 
 export const AppDataSource = new DataSource({
-    type : DB_TYPE,
-    host: DB_HOST,
-    port:  DB_PORT,
-    username: DB_USERNAME,
-    password: DB_PASSWORD,
-    database: DB_DATABASE,
-    synchronize:DB_SYNC ,
+    type: "postgres",
+    
+
+    url: process.env.DB_URL,
+    synchronize: DB_SYNC,
     logging: DB_LOGGING,
     entities: DB_ENTITES,
-    dropSchema : DB_DROP,
+    dropSchema: DB_DROP,
+    
+    ssl: {
+      rejectUnauthorized: false,
+    },
+
 })
+
 
  export const Usermodel: Repository<User> = AppDataSource.getRepository(User)
  export const CredentialModel: Repository<Credential> = AppDataSource.getRepository(Credential)
